5a075063c4bb64c30b2b1617169120c964dab0ee
joniborda <jonathanmatiasborda@gmail.com>
2017-08-02 22:41:45 -0300

    empiezo a leer archivo

--- src/principal/herramientas/CargadorRecursos.java
+++ src/principal/herramientas/CargadorRecursos.java
@@ -6,7 +6,10 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.awt.Transparency;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
 
@@ -58,5 +61,35 @@ public class CargadorRecursos {
 		g.drawImage(imagen, 0, 0, null);
 		g.dispose();
 		return imagenAcelerada;
+	}
+
+	public static String leerArchivoTexto(final String ruta) {
+
+		String contenido = "";
+
+		InputStream entradaBytes = ClassLoader.class.getResourceAsStream(ruta);
+
+		BufferedReader lector = new BufferedReader(new InputStreamReader(entradaBytes));
+
+		String linea;
+		try {
+			while ((linea = lector.readLine()) != null) {
+				contenido += linea;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (entradaBytes != null) {
+					entradaBytes.close();
+				}
+				if (lector != null) {
+					lector.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return contenido;
 	}
 }
--- src/principal/maquinaestado/estados/juego/GestorJuego.java
+++ src/principal/maquinaestado/estados/juego/GestorJuego.java
@@ -1,14 +1,16 @@
 package principal.maquinaestado.estados.juego;
 
+import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.image.BufferedImage;
 
+import principal.herramientas.CargadorRecursos;
 import principal.maquinaestado.EstadoJuego;
 
 public class GestorJuego implements EstadoJuego {
 
 	private GestorMapa gm;
 
+	String texto = CargadorRecursos.leerArchivoTexto("/texto/prueba");
 	// HojaSprites hs = new HojaSprites("/imagenes/hojasTexturas/desierto.png",
 	// 32, true);
 
@@ -19,6 +21,9 @@ public class GestorJuego implements EstadoJuego {
 	public void dibujar(Graphics g) {
 		// BufferedImage imagen = hs.getSprite(3, 3).getImagen();
 		// g.drawImage(imagen, 100, 100, null);
+
+		g.setColor(Color.white);
+		g.drawString(texto, 10, 10);
 	}
 
 }
